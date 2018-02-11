c4ecd19c9dca92c644132964c3baf37bdb31e7e4
Addie Guo <addieguo98@gmail.com>
2018-02-11 01:47:53 -0500

    backend computation

--- appv1.js
+++ appv1.js
@@ -1,42 +1,9 @@
 const express = require('express') 
 const app = express()
 var path = require("path");
-var bodyParser = require('body-parser');
 var spotcrime = require('spotcrime');
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false}));
-// var spotcrime = require('spotcrime');
-
-app.post('/test', function(req, res) {
-
-
-
-    console.log(JSON.stringify(req.body));
-
-    let lat = parseFloat(req.body['lat']);
-    let lng = parseFloat(req.body['lng']);
-
-    res.json([
-        [lat],
-        [lng],
-        []
-    ]);
-});
-
-app.get('/', function(req, res){
-    res.sendFile(path.join(__dirname+'/index.html'));
-});
-
-app.get('/bootstrap.min.css', function(req, res){
-    res.sendFile(path.join(__dirname+'/bootstrap.min.css'));
-});
-
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
-
-
-// ----------BACKEND COMPUTATIONS-------------
-/ variables that need to be connected to JSON from Alex
+// variables that need to be connected to JSON from Alex
 var currX;
 var currY;
 var newX = 31;
@@ -165,5 +132,4 @@ function findShortestPath() {
 
 // Figure out how to add path to map --> Google API?
 // maxCrime/7 
-
 
