eeb7141971f28ac25038c50d841b4606c944e90e
Alexandrea Livings <alivings@fingerlakes.edu>
2018-02-11 00:58:31 -0500

    fixed bootstrap.css from server

--- app.js
+++ app.js
@@ -8,4 +8,8 @@ app.get('/', function(req, res){
     res.sendFile(path.join(__dirname+'/index.html'));
 });
 
+app.get('/bootstrap.min.css', function(req, res){
+    res.sendFile(path.join(__dirname+'/bootstrap.min.css'));
+});
+
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
