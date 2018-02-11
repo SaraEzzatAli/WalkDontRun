855597bb60c162063a4524de4d40819c529c2da3
Alexandrea Livings <alivings@fingerlakes.edu>
2018-02-11 01:36:25 -0500

    fixed location post request

--- app.js
+++ app.js
@@ -1,8 +1,27 @@
 const express = require('express') 
 const app = express()
 var path = require("path");
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false}));
 // var spotcrime = require('spotcrime');
 
+app.post('/test', function(req, res) {
+
+
+
+    console.log(JSON.stringify(req.body));
+
+    let lat = parseFloat(req.body['lat']);
+    let lng = parseFloat(req.body['lng']);
+
+    res.json([
+        [lat],
+        [lng],
+        []
+    ]);
+});
 
 app.get('/', function(req, res){
     res.sendFile(path.join(__dirname+'/index.html'));
