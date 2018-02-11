7dcfc04fba5b37c7a77b85595fd00494084ae227
Alexandrea Livings <Alexandrea Livings>
2018-02-10 20:43:51 -0500

    another commit

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express') 
 const app = express()
 var path = require("path");
+// var spotcrime = require('spotcrime');
 
 
 app.get('/', function(req, res){
-    res.sendFile(path.join(__dirname+'/map.html'));
+    res.sendFile(path.join(__dirname+'/index.html'));
 });
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
